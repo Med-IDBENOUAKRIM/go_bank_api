57fb0481371d0d0a56c09f818e0d1f073dfed9c0
Med-IDBENOUAKRIM <medidbenouakrim@gmail.com>
2023-02-05 20:58:50 +0100

    first commint

---
+++ api.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func toJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type ApiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHttpHandleFunc(f ApiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			toJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
+}
+
+type ApiServer struct {
+	port_address string
+}
+
+func NewServer(port string) *ApiServer {
+	return &ApiServer{
+		port_address: port,
+	}
+}
+
+func (s *ApiServer) RunServer() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHttpHandleFunc(s.handleAccount))
+	log.Printf("The server start on: %+v", s.port_address)
+	http.ListenAndServe(s.port_address, router)
+
+}
+
+func (s *ApiServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("Error: %s", r.Method)
+}
+
+func (s *ApiServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *ApiServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}

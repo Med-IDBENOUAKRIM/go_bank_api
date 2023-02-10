4a09e201c3d04976afab112c6b1b823efee631ff
Med-IDBENOUAKRIM <medidbenouakrim@gmail.com>
2023-02-10 15:09:11 +0100

    first commit

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
 )
 
+func seedAccount(store Storage, first_name, last_name, password string) *Account {
+	account, err := NewAccount(first_name, last_name, password)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := store.CreateAccount(account); err != nil {
+		log.Fatalln("Seeding accounts got error : ", err)
+	}
+
+	return account
+}
+
 func main() {
+	seed := flag.Bool("seed", false, "seed th database")
+	flag.Parse()
+
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
@@ -12,6 +29,11 @@ func main() {
 
 	if err := store.Init(); err != nil {
 		log.Fatal(err)
+	}
+
+	if *seed {
+		log.Println("seeding the database")
+		seedAccount(store, "Mohamed", "Idben", "pass123")
 	}
 
 	server := NewServer(":3550", store)
